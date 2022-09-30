1595a63b7910f2c26bd0e45c540df5c18790c97c
kunal <kunal@kunal>
2022-09-30 14:14:11 +0530

    29.09.2022 lab

---
+++ Lab29sept/src/Quation2/EvenOdd.java
@@ -0,0 +1,37 @@
+package Quation2;
+
+import java.util.Scanner;
+
+public class EvenOdd {
+	
+static void num(int...EvenOdd) {
+		  
+		   for(int i : EvenOdd)
+		   {
+			   System.out.print(i+ "  ");
+		   }
+		    System.out.println(" Number is ");
+	  
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// input method
+		
+		Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        // if condition
+        
+        if(num % 2 == 0)
+            System.out.println(num + " is even");
+        // Else condition
+        else
+            System.out.println(num + " is odd");
+        sc.close();
+    }
+
+}
